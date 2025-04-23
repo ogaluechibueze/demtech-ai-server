93f1ba64c9f66edf092ff20d6fd549a7aa573443
ogaluechibueze <ogaluechibueze@yahoo.com>
2025-04-23 22:33:12 +0100

    new

--- index.js
+++ index.js
@@ -157,7 +157,11 @@ app.use((err, req, res, next) => {
 });
 
 // PRODUCTION
+app.use(express.static(path.join(__dirname, "dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(port, () => {
   connect();
